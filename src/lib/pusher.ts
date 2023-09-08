54de6423a597b42175c7bbc2d8008ded0d4115c9
MuhammadUsamaAwan <underusama@gmail.com>
2023-09-08 20:21:52 +0500

    remove auth

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -6,23 +6,8 @@ export const pusherServer = new PusherServer({
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
-  useTLS: true,
 });
-
-/**
- * The following pusher client uses auth, not neccessary for the video chatroom example
- * Only the cluster would be important for that
- * These values can be found after creating a pusher app under
- * @see https://dashboard.pusher.com/apps/<YOUR_APP_ID>/keys
- */
 
 export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
-  // authEndpoint: '/api/pusher-auth',
-  authTransport: 'ajax',
-  auth: {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  },
 });
