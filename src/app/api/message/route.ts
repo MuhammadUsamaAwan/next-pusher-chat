24154fe637685b559ec7be7de73f52158dbee4ed
MuhammadUsamaAwan <underusama@gmail.com>
2023-09-08 18:13:30 +0500

    possible prod fix

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -3,7 +3,7 @@ import { pusherServer } from '@/lib/pusher';
 export async function POST(req: Request) {
   const { text, roomId } = await req.json();
 
-  pusherServer.trigger(roomId, 'incoming-message', text);
+  await pusherServer.trigger(roomId, 'incoming-message', text);
 
   return new Response(JSON.stringify({ success: true }));
 }
